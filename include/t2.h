395f925600bae919d8e485798e2ad3f1a98dd2a5
HadiElnemr <hadi.elnemr@gmail.com>
2024-05-26 14:52:53 +0200

    add header files for tasks

---
+++ include/t2.h
@@ -0,0 +1,49 @@
+#ifndef T2_H
+#define T2_H
+
+#include <iostream>
+#include <osmium/io/pbf_input.hpp>
+
+#include <osmium/util/file.hpp>
+#include <osmium/util/progress_bar.hpp>
+
+#include <osmium/osm.hpp>
+#include <osmium/io/any_input.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+
+#include <osmium/geom/coordinates.hpp>
+#include <osmium/geom/geojson.hpp>
+#include <osmium/geom/mercator_projection.hpp> // Include MercatorProjection
+#include <osmium/geom/wkt.hpp> // Include WKTFactory
+
+
+#include <fstream>
+#include <iostream>
+
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+
+
+void merge_touching_ways_nestedloop(std::vector<std::vector<osmium::object_id_type>> &ways);
+void merge_touching_ways(std::vector<std::vector<osmium::object_id_type>> &unclosed_ways,
+                         std::vector<std::vector<osmium::object_id_type>> &stitched_ways,
+                         bool closed_ways_only);
+int read_coastline_ways(std::string &inputFile,
+                        std::vector<std::vector<osmium::object_id_type>> &ways_collection);
+void extract_nodes_from_ways(std::vector<std::vector<osmium::object_id_type>> &ways_collection,
+                             std::unordered_set<osmium::object_id_type> &node_ids);
+int read_coastline_nodes(std::string &inputFile,
+                         std::unordered_set<osmium::object_id_type> &node_ids,
+                         std::unordered_map<osmium::object_id_type, osmium::Location> &node_locations);
+void print_geojson_nodes(std::unordered_map<osmium::object_id_type, osmium::Location> &nodes);
+int write_geojson_nodes(std::string &outputFilePath,
+                        std::unordered_map<osmium::object_id_type, osmium::Location> &nodes);
+void get_closed_ways(std::vector<std::vector<osmium::object_id_type>> &ways_collection,
+                     std::vector<std::vector<osmium::object_id_type>> &closed_ways);
+void get_unclosed_ways(std::vector<std::vector<osmium::object_id_type>> &ways_collection,
+                       std::vector<std::vector<osmium::object_id_type>> &unclosed_ways);
+int main_task2(int argc, char* argv[]);
+
+#endif // T2_H
---
+++ include/t3.h
@@ -0,0 +1,27 @@
+#ifndef T3_H
+#define T3_H
+
+#include <iostream>
+#include <osmium/io/pbf_input.hpp>
+
+#include <osmium/geom/coordinates.hpp>
+#include <osmium/geom/geojson.hpp>
+#include <osmium/geom/mercator_projection.hpp> // Include MercatorProjection
+
+#include <cmath>
+
+#include <unordered_map>
+
+
+void get_final_closed_ways(std::string &input_file_name, std::vector<std::vector<osmium::object_id_type>> &final_closed_ways, std::unordered_map<osmium::object_id_type, osmium::Location> &node_locations);
+void getBoundingBox(std::vector<std::vector<osmium::object_id_type>> &closed_ways, std::unordered_map<osmium::object_id_type, osmium::Location> &node_locations, std::vector<std::pair<osmium::Location, osmium::Location>> &bounding_boxes);
+double latitude(double lambda, double lambda1, double lambda2, double phi1, double phi2);
+double latitude_crossing(double lambda, double lambda1, double lambda2, double phi1, double phi2);
+bool isWater(osmium::Location &Q,
+            std::vector<std::pair<osmium::Location, osmium::Location>> &bounding_boxes,
+            std::unordered_map<osmium::object_id_type,
+            osmium::Location> &node_locations);
+
+int main_task3(int argc, char* argv[]);
+
+#endif // T3_H

740ead0f83b5900e6eaf92b9063a3be804f13837
HadiElnemr <hadi.elnemr@gmail.com>
2024-06-01 21:19:05 +0200

    refactor and some runtime computations

--- src/t3.cpp
+++ src/t3.cpp
@@ -1,15 +1,10 @@
 #include "t3.h"
 #include "t2.h"
 
-// #include <cmath>
 
-// osmium::Location spherical_polygon_centre(std::vector<osmium::Location> closed_way){
-//     for(int i = 0; i<closed_way.size(); i++){
-//         auto a = closed_way[i];
-//         auto b = closed_way[(i+1)%closed_way.size()];
-//         auto v = a.cross(b);
-//     }
-// }
+#include <chrono> // for execution time
+
+std::vector<osmium::Location> crossed_nodes;
 
 void get_final_closed_ways(std::string &input_file_name,
                            std::vector<std::vector<osmium::object_id_type>> &final_closed_ways,
@@ -18,13 +13,24 @@ void get_final_closed_ways(std::string &input_file_name,
     std::vector<std::vector<osmium::object_id_type>> ways_collection;
     std::unordered_set<osmium::object_id_type> node_ids;
 
-    // std::vector<std::vector<osmium::object_id_type>> stitched_ways;
+    // read_coastline_ways(input_file_name, ways_collection);
+    // std::cout << "Coastline Ways count: " << ways_collection.size() << std::endl;
+    // extract_nodes_from_ways(ways_collection, node_ids);
+    // std::cout << "Nodes extracted: " << node_ids.size() << std::endl;
 
-    read_coastline_ways(input_file_name, ways_collection);
-    std::cout << "Coastline Ways count: " << ways_collection.size() << std::endl;
-    extract_nodes_from_ways(ways_collection, node_ids);
-    std::cout << "Nodes extracted: " << node_ids.size() << std::endl;
+    auto t1 = std::chrono::high_resolution_clock::now();
+    read_coastline_ways_and_node_ids(input_file_name, ways_collection, node_ids);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    std::cout << "time for read_coastline_ways_and_node_ids: " << ms_int.count() << std::endl;
+
+    t1 = std::chrono::high_resolution_clock::now();
     read_coastline_nodes(input_file_name, node_ids, node_locations);
+    t2 = std::chrono::high_resolution_clock::now();
+    ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    std::cout << "time for read_coastline_nodes: " << ms_int.count() << std::endl;
+
     std::cout << "Coastline Nodes count: " << node_locations.size() << std::endl;
 
     node_ids.clear();
@@ -78,34 +84,9 @@ float latitude_crossing_X(osmium::Location Q, float lambda1, float lambda2, floa
     return phi1 + (Q.lat() - lambda1) / (lambda2 - lambda1) * (phi2 - phi1);
 }
 
-// bool pointInBox(osmium::Location &Q, osmium::Location &A, osmium::Location &B){
-//     return Q.lat() >= A.lat() && Q.lat() <= B.lat() && Q.lon() >= A.lon() && Q.lon() <= B.lon();
-//     // Q.lat() < A.lat() || Q.lat() > B.lat() || Q.lon() < A.lon() || Q.lon() > B.lon()
-// }
-
-// greatCircleIntersection(great_circle, osmium::Location &node){
-
-// }
-
-// int countRayIntersections(const SphericalPoint& origin,
-//                           const SphericalPoint& direction,
-//                           const std::vector<osmium::Location>& way)
-// {
-
-//   // Define a great circle for the ray (origin and direction define the plane)
-//   // ... (code to define the great circle)
-
-//   int intersection_count = 0;
-//   for (const auto& node : way) {
-//     std::vector<osmium::Location> intersections;
-//     greatCircleIntersection(great_circle, node);
-//     intersection_count += intersections.size();
-//   }
-// //   return intersection_count;
-// }
-
-
-
+bool pointInBox(osmium::Location &Q, osmium::Location &A, osmium::Location &B){
+    return Q.lat() >= A.lat() && Q.lat() <= B.lat() && Q.lon() >= A.lon() && Q.lon() <= B.lon();
+}
 
 ///////////////////////////////////////   By Darel Rex Finley
 // bool isEdgeCrossedByHorizontalLine(osmium::Location Q, osmium::Location i, osmium::Location j){
@@ -124,7 +105,12 @@ float latitude_crossing_X(osmium::Location Q, float lambda1, float lambda2, floa
 // }
 
 // isEdgeCrossed by northerly ray to the North Pole
-bool isEdgeCrossed(osmium::Location Q, osmium::Location i, osmium::Location j){
+bool isEdgeCrossed(osmium::Location& Q, osmium::Location& i, osmium::Location& j){
+    if( ( (i.lon() < Q.lon() && j.lon() >= Q.lon()) || (j.lon() < Q.lon() && i.lon() >= Q.lon()) ) // if Q is between edge longitudes    i    Q     j
+        &&  (i.lat() >= Q.lat() || j.lat() >= Q.lat())
+        && i.lat() + (j.lat() - i.lat()) / (j.lon() - i.lon()) * (Q.lon() - i.lon()) > Q.lat())
+        crossed_nodes.push_back(osmium::Location(Q.lon(), i.lat() + (j.lat() - i.lat()) / (j.lon() - i.lon()) * (Q.lon() - i.lon())));
+
     if( ( (i.lon() < Q.lon() && j.lon() >= Q.lon()) || (j.lon() < Q.lon() && i.lon() >= Q.lon()) ) // if Q is between edge longitudes    i    Q     j
         &&  (i.lat() >= Q.lat() || j.lat() >= Q.lat())) // if the edge is above Q
             return (
@@ -150,34 +136,15 @@ bool isBoundingBoxCrossed(osmium::Location &Q, std::pair<osmium::Location, osmiu
     return isEdgeCrossed(Q, p1, p2) ^ isEdgeCrossed(Q, p2, p3) ^ isEdgeCrossed(Q, p3, p4) ^ isEdgeCrossed(Q, p4, p1);
 }
 
-////////////////////////////////////////
-//                                                             instead
-//  int    polyCorners  =  how many corners the polygon has    -------> size() of the way
-//  float  polyX[]      =  horizontal coordinates of corners   -------> lon
-//  float  polyY[]      =  vertical coordinates of corners     -------> lat
-//  float  x, y         =  point to be tested                  -------> Q
-
 bool isPointInPolygon(osmium::Location &Q,
                     std::vector<osmium::object_id_type> &way,
                     std::unordered_map<osmium::object_id_type, osmium::Location> &node_locations) {
 
-  auto j = way.size()-1;
+//   auto j = way.size()-1;
+  auto j = 0;
   bool parity_odd = 0;
 
-    for(std::size_t i = 0; i < way.size(); i++) {
-        // if( (way[i].lat() < Q.lat() && way[j].lat() >= Q.lat() ||   way[j].lat() < Q.lat() && way[i].lat() >= Q.lat()) // if Q is between edge lats
-        // &&  (way[i].lon() <= Q.lon() || way[j].lon() <= Q.lon())) // if Q is to the right of the edge
-        // {
-        //     parity_odd ^= (                                   // XOR
-        //     way[i].lon()
-        //     +
-        //     (Q.lat() - way[i].lat()) / (way[j].lat() - way[i].lat())
-        //     *
-        //     (way[j].lon() - way[i].lon())
-        //     <
-        //     Q.lon()
-        //     );
-        // }
+    for(std::size_t i = 1; i < way.size(); i++) {
         parity_odd ^= isEdgeCrossed(Q, node_locations[way[i]], node_locations[way[j]]);
         j = i;
 }
@@ -197,39 +164,30 @@ bool isWater(osmium::Location &Q,
     std::vector<unsigned int> crossed_ways_bounding_boxes_indices;
     // Check if the casting ray crosses the bounding box
     for(std::size_t i = 0; i<bounding_boxes.size(); i++){
-        // osmium::Location A = bounding_boxes[i].first; // SW          ......B
-        // osmium::Location B = bounding_boxes[i].second; // NE         A......
-
         //////////////////////////////
-        // To be commented
         // if(pointInBox(Q, A, B)){
         //     Q_bounding_boxes.push_back(std::make_pair(A, B));
         //     matched_ways_bounding_boxes_indices.push_back(i);
         // }
         //////////////////////////////
 
-        if(isBoundingBoxCrossed(Q, bounding_boxes[i])){
-            // crossed_bounding_boxes.push_back(bounding_boxes[i]);
+        if(pointInBox(Q, bounding_boxes[i].first, bounding_boxes[i].second))
             crossed_ways_bounding_boxes_indices.push_back(i);
-        }
     }
 
-    // if(crossed_ways_bounding_boxes_indices.size() == 0) // miscillaneous, already won't enter the loop if false
-    //     return true;
-
-    // further investigation
     std::cout << "\nCrossed bounding boxes: " << crossed_ways_bounding_boxes_indices.size() << std::endl;
 
 
     // Comment
     bool parity_odd = 0;
     for(std::size_t i = 0; i<closed_ways.size(); i++){
-        parity_odd ^= isPointInPolygon(Q, closed_ways[i], node_locations);                                // if Q is in the closed way "land", then naturlich Land
-        // if(isPointInPolygon(Q, closed_ways[i], node_locations))                                // if Q is in the closed way "land", then naturlich Land
-        //     return false;   // Wrong
+        if(isPointInPolygon(Q, closed_ways[i], node_locations))                                // if Q is in the closed way "land", then naturlich Land
+            return false;
+        // parity_odd ^= isPointInPolygon(Q, closed_ways[i], node_locations);                                // if Q is in the closed way "land", then naturlich Land
+
     }
 
-    //Uncomment
+    //Uncomment // but change to boxes bounding the test point
     // for(std::size_t i = 0; i<crossed_ways_bounding_boxes_indices.size(); i++){
     //     // std::vector<osmium::object_id_type> way = closed_ways[crossed_ways_bounding_boxes_indices[i]];
     //     if(isPointInPolygon(Q,
@@ -240,51 +198,92 @@ bool isWater(osmium::Location &Q,
 
     // std::cout << "\nNot inside any Land" << std::endl;
 
-    return parity_odd;
+    return 1^parity_odd;
 }
-
-// float way_length(std::vector<osmium::object_id_type> &way,
-//                  std::unordered_map<osmium::object_id_type, osmium::Location> &node_locations)
-// {
-//     float length = 0;
-//     for(std::size_t i = 0; i<way.size()-1; i++){
-//         length += node_locations[way[i]].distance_to(node_locations[way[i+1]]);
-//     }
-//     return length;
-// }
 
 int main_task3(int argc, char* argv[])
 {
+    std::string input_file_name;
+
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " INPUT-FILE\n";
-        return 1;
+        // return 1;
+        input_file_name = "antarctica-latest.osm.pbf";
     }
+    else
+        input_file_name = argv[1];
 
-    std::string input_file_name = argv[1];
     std::vector<std::vector<osmium::object_id_type>> final_closed_ways;
     std::unordered_map<osmium::object_id_type, osmium::Location> node_locations;
+
     get_final_closed_ways(input_file_name, final_closed_ways, node_locations);
+//          |
+//          |
+//          ^
+    // // Loading is faster::
+    // std::ifstream infile_final_closed("outfile-final_closed.dat", std::ifstream::binary);
+    // final_closed_ways.resize(625855); //625855
+    // infile_final_closed.read(reinterpret_cast<char*>(final_closed_ways.data() /* or &v[0] pre-C++11 */), sizeof(int) * 625855);
+    // final_closed_ways.shrink_to_fit();
+    // read_coastline_nodes(input_file_name, node_ids, node_locations);
+    // infile_final_closed.close();
+
+    std::cout << node_locations.size() << std::endl;
+    std::cout << "test in bounding boxes" << std::endl;
 
     std::vector<std::pair<osmium::Location, osmium::Location>> bounding_boxes;
     getBoundingBox(final_closed_ways, node_locations, bounding_boxes);
     std::cout << "number of bounding boxes: " << bounding_boxes.size() << std::endl;
     // final_closed_ways.clear();
 
+    std::cout << "Not in bounding boxes" << std::endl;
+
+
+    ////////////////////////////////////////////////////////////// testing saving vector
+
+    // std::ofstream outfile_final_closed("outfile-final_closed.dat", std::ofstream::binary);
+    // // std::ofstream outfile_node_locations("outfile-node_locations.dat", std::ofstream::binary);
+    // outfile_final_closed.write(reinterpret_cast<const char*>(final_closed_ways.data() /* or &v[0] pre-C++11 */), sizeof(int) * final_closed_ways.size());
+    // // outfile_node_locations.write(reinterpret_cast<const char*>(node_locations.data() /* or &v[0] pre-C++11 */), sizeof(int) * node_locations.size());
+    // outfile_final_closed.close();
+
+    // read file again
+    // std::ifstream infile_final_closed("outfile-final_closed.dat", std::ifstream::binary);
+    // std::vector<std::vector<osmium::object_id_type>> final_closed_ways2;
+
+    // final_closed_ways2.resize(626000); // 625855
+    // infile_final_closed.read(reinterpret_cast<char*>(final_closed_ways2.data() /* or &v[0] pre-C++11 */), sizeof(int) * final_closed_ways2.size());
+    // final_closed_ways2.shrink_to_fit();
+
+    // std::cout << "Reading......................................" << std::endl;
+    // read_coastline_nodes(input_file_name, node_ids, node_locations2);
+    // std::cout << "Finished......................................" << std::endl;
+
+    //////////////////////////////////////////////////////////////  end testing saving vector
+
+
+
+
     std::vector<osmium::Location> test_points;
-    test_points.push_back(osmium::Location(-26.732548749488956, 10.671596281947856));
-    test_points.push_back(osmium::Location(11.939290077225365, 12.162589122776751));
-    test_points.push_back(osmium::Location(16.193990227322075, -80.31058318376975));
+    // test_points.push_back(osmium::Location(-26.732548749488956, 10.671596281947856));
+    // test_points.push_back(osmium::Location(11.939290077225365, 12.162589122776751));
+    // test_points.push_back(osmium::Location(16.193990227322075, -80.31058318376975));
     test_points.push_back(osmium::Location(-23.30866463537828, -85.05112877980659)); // In Antarctica
-    test_points.push_back(osmium::Location(122.32121620632046, -72.40337730584133)); // In Antarctica other side
-    test_points.push_back(osmium::Location(61.6524702239368, 76.2930632732388)); // In Russia
-    test_points.push_back(osmium::Location(-38.48528429954149, 77.52013957622279)); // In Greenland
-    test_points.push_back(osmium::Location(-89.46894116718542, 85.05112877980659)); // In Water near north pole
+    // test_points.push_back(osmium::Location(122.32121620632046, -72.40337730584133)); // In Antarctica other side
+    // test_points.push_back(osmium::Location(61.6524702239368, 76.2930632732388)); // In Russia
+    // test_points.push_back(osmium::Location(-38.48528429954149, 77.52013957622279)); // In Greenland
+    // test_points.push_back(osmium::Location(-89.46894116718542, 85.05112877980659)); // In Water near north pole
+    // test_points.push_back(osmium::Location(-183.2858570662184, 81.88365705783642)); // In Water near north pole  // Doesn't work, throws an error: osmium::invalid_location
+    // test_points.push_back(osmium::Location(161.17794752875415, 5.768768463425289)); // In Water Pacific Ocean
+    // test_points.push_back(osmium::Location(-190.18369831686493, 82.69225385345266)); // In Water North pole   // error: osmium::invalid_location
 
     for(auto Q : test_points){
         std::cout << Q << std::endl;
         std::cout << (isWater(Q, bounding_boxes, final_closed_ways, node_locations)? "----Water----" : "----Land----") << std::endl;
     }
-    // std::cout << isEdgeCrossed(osmium::Location(3,2), osmium::Location(2,3), osmium::Location(4,3)) << std::endl;
+    for(auto location: crossed_nodes)
+        std::cout << location << std::endl;
+    // std::cout << isEdgeCrossed(osmium::Location(3.0,2.0), osmium::Location(2,3), osmium::Location(4,3)) << std::endl;
 
     return 0;
 }
